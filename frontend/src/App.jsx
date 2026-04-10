9156ebbf647a574146d42a87012084a13502dee3
devhub201 <cubeisyiv@gmail.com>
2026-04-10 19:46:20 +0530

    Delete frontend directory

--- frontend/src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{color: "white", textAlign: "center", marginTop: "100px"}}>
-      <h1>HostAI 🚀</h1>
-      <p>Frontend setup working</p>
-    </div>
-  );
-}
