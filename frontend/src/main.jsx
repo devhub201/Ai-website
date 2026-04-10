8aeebdfc067b64968469604ec004841cf5150d17
devhub201 <cubeisyiv@gmail.com>
2026-04-10 21:17:25 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./themes/aqua/theme.css";
 
-const root = document.getElementById("root");
-
-if (root) {
-  ReactDOM.createRoot(root).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+function Test() {
+  return <h1 style={{color:"white"}}>WORKING</h1>;
 }
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Test />
+);
